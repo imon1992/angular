e8cd36f014b3caedd573ad762454c7846361fd27
imon1992 <andrey.kolotii@mail.ru>
2017-10-24 22:26:18 +0300

    day2

--- app/main.js
+++ app/main.js
@@ -1,30 +1,22 @@
-angular.module("app",[]);//sozdali novii modul
-angular.module("app").controller('SettingsController1', SettingsController1);
-angular.module("app").controller('SettingsController2', SettingsController2);
-function SettingsController1($scope) { //srv modul ili server kotorii uje est
-    $scope.name = 'Vsya';
-    this.name = 'JOhn Smith';
-}
-function SettingsController2($scope) { //srv modul ili server kotorii uje est
-    var self = this;
-    this.names = [];
-    $scope.names = []
-    this.addName = function(fname){
-        self.names.push(fname);
-        $scope.names.push(fname);
+var app = angular.module('app', [])
+
+app.controller('userController', function (nameStorage) {
+    this.addNames = function(name){
+        nameStorage.setName(name);
     }
-
-    this.deleteName = function(id){
-        self.names.splice(id,1);
+})
+    .service('nameStorage', function () {
+    var names = [];
+    return {
+        setName: function (name) {
+            names.push(name);
+        },
+        getName: function () {
+            return names;
+        }
     }
+});
 
-    this.friends=[
-        {name: 'John',   phone: '555-1212',  age: 10},
-        {name: 'Mary',   phone: '555-9876',  age: 19},
-        {name: 'Mike',   phone: '555-4321',  age: 21},
-        {name: 'Adam',   phone: '555-5678',  age: 35},
-        {name: 'Julie',  phone: '555-8765',  age: 29}
-      ];
-
-      this.list=['Hell','world','angular','vue','php'];
-}
+app.controller('getNames',function(nameStorage){
+    this.names= nameStorage.getName();
+})
